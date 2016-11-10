76549d9a2e7aed59409a241fe8c51b999edf070e
jecoulte <jecoulte@iu.edu>
2016-11-10 13:25:12 -0500

    un-echoed destroy script

--- destroy.sh
+++ destroy.sh
@@ -3,6 +3,6 @@
 openrc_loc=./openrc.sh #openrc.sh file - see Jetstream docs!
 stack_name=$(grep 'stack_name=' deploy.sh | sed -e 's/.*"\(.*\)".*/\1/')
 
-echo "source $openrc_loc"
+source $openrc_loc
 
-echo "openstack stack delete $stack_name"
+openstack stack delete $stack_name
