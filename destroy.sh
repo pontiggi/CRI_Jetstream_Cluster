5127b988b7475d46a2dd88ca391dcbefc47ffeb2
jecoulte <jecoulte@iu.edu>
2016-11-10 19:37:31 -0500

    added until loop to destroy

--- destroy.sh
+++ destroy.sh
@@ -6,3 +6,10 @@ stack_name=$(grep 'stack_name=' deploy.sh | sed -e 's/.*"\(.*\)".*/\1/')
 source $openrc_loc
 
 openstack stack delete $stack_name
+
+stack_delete_status=$(openstack stack show -c stack_status $stack_name | awk '/stack_status/ {print $4}')
+until [[ $stack_delete_status != "DELETE_IN_PROGRESS" ]]; do
+ echo "Deleting stack..."
+ sleep 30
+ stack_delete_status=$(openstack stack show -c stack_status $stack_name | awk '/stack_status/ {print $4}')
+done
